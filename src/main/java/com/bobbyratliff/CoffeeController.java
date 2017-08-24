dab8d4a3b015d821894d9f7426eda71c09daccfb
Bobby Ratliff <ratliff.bobby@gmail.com>
2017-08-23 21:20:56 -0500

    Add more methods of getting Coffee.

--- src/main/java/com/bobbyratliff/CoffeeController.java
+++ src/main/java/com/bobbyratliff/CoffeeController.java
@@ -1,7 +1,9 @@
 package com.bobbyratliff;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("/Coffee")
@@ -11,9 +13,26 @@ public class CoffeeController {
     public Response getCoffeeCup() {
         CoffeeCup keepCalm = new CoffeeCup();
         keepCalm.message = "Keep Calm and Carry On";
+        keepCalm.countryOfOrigin = "Britain";
         keepCalm.color = "red";
         keepCalm.numHandles = 1;
         return Response.ok(keepCalm).build();
+    }
+
+    @POST
+    public Response customMessage(@QueryParam("message") String message) {
+        CoffeeCup cup = new CoffeeCup();
+        cup.message = message;
+        cup.countryOfOrigin = "China";
+        cup.color = "red";
+        cup.numHandles = 1;
+        return Response.ok(cup).build();
+    }
+
+    @POST
+    @Path("/Cup")
+    public Response echoCoffee(CoffeeCup cup) {
+        return Response.ok(cup).build();
     }
 
     static class CoffeeCup {
