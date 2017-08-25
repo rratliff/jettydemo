8cd87bff9f54fdb274817f335d46e14cd809d291
Bobby Ratliff <ratliff.bobby@gmail.com>
2017-08-24 20:32:39 -0500

    Update React app to call the api using jQuery.
    Simple React state and a form.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {coffee: [], message: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.Coffee();
+  }
+
+  handleChange(event) {
+    this.setState({message: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.Coffee(this.state.message);
+    event.preventDefault();
+  }
+
+  Coffee(message) {
+    return $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: 'api/Coffee?' + $.param({message: message})
+    }).then((data) => {
+      console.log(data);
+      this.setState({coffee: data});
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,8 +44,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Message:
+            <input type="text" value={this.state.message} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.coffee.message}
         </p>
       </div>
     );
